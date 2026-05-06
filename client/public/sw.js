11fe484ce95e57bbed47e40f51340135a1c2ae27
Pritam Patel <127683495+PritamPatelLoyadhami@users.noreply.github.com>
2026-05-06 16:56:22 +0530

    feat: Added PWA manifest and service worker to make mobile camera installable

---
+++ client/public/sw.js
@@ -0,0 +1,13 @@
+// Basic Service Worker for PWA installability
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let the browser do its default thing for now
+  // We just need a registered service worker with a fetch handler to pass PWA criteria
+});
